d988ae342bc9e67f4c2d6b2756e78a27c8690c49
valentin1042 <valentin1042@gmail.com>
2016-04-25 16:05:32 -0500

    Changed start up
    Removed form1
    Changed startup form to mainMenuform

--- t3Final/t3Final/My Project/Application.Designer.vb
+++ t3Final/t3Final/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.t3Final.Form1
+            Me.MainForm = Global.t3Final.mainMenuForm
         End Sub
     End Class
 End Namespace

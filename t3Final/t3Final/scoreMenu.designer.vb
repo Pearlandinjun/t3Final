e85d14155bf494bed04bc27c8976fee415fbd5b1
valentin1042 <valentin1042@gmail.com>
2016-04-27 13:22:17 -0500

    modified scoremenuform
    added new scoremenuform

--- t3Final/t3Final/scoreMenu.designer.vb
+++ t3Final/t3Final/scoreMenu.designer.vb
@@ -25,7 +25,12 @@ Partial Class scoreMenuForm
         Me.Btn_View = New System.Windows.Forms.Button()
         Me.btn_Clear = New System.Windows.Forms.Button()
         Me.btn_Back = New System.Windows.Forms.Button()
-        Me.List_Score = New System.Windows.Forms.ListBox()
+        Me.historyTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Btn_View
@@ -60,15 +65,64 @@ Partial Class scoreMenuForm
         Me.btn_Back.Text = "Back"
         Me.btn_Back.UseVisualStyleBackColor = True
         '
-        'List_Score
+        'historyTextBox
         '
-        Me.List_Score.FormattingEnabled = True
-        Me.List_Score.ItemHeight = 16
-        Me.List_Score.Location = New System.Drawing.Point(0, 0)
-        Me.List_Score.Margin = New System.Windows.Forms.Padding(4)
-        Me.List_Score.Name = "List_Score"
-        Me.List_Score.Size = New System.Drawing.Size(309, 164)
-        Me.List_Score.TabIndex = 3
+        Me.historyTextBox.Enabled = False
+        Me.historyTextBox.Location = New System.Drawing.Point(16, 98)
+        Me.historyTextBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.historyTextBox.Name = "historyTextBox"
+        Me.historyTextBox.Size = New System.Drawing.Size(328, 22)
+        Me.historyTextBox.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 75)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(72, 75)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "W"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(105, 75)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "T"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(0, 0)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(133, 75)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(16, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "L"
         '
         'scoreMenuForm
         '
@@ -77,7 +131,12 @@ Partial Class scoreMenuForm
         Me.CancelButton = Me.btn_Back
         Me.ClientSize = New System.Drawing.Size(379, 321)
         Me.ControlBox = False
-        Me.Controls.Add(Me.List_Score)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.historyTextBox)
         Me.Controls.Add(Me.btn_Back)
         Me.Controls.Add(Me.btn_Clear)
         Me.Controls.Add(Me.Btn_View)
@@ -86,10 +145,16 @@ Partial Class scoreMenuForm
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Score"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Btn_View As System.Windows.Forms.Button
     Friend WithEvents btn_Clear As System.Windows.Forms.Button
     Friend WithEvents btn_Back As System.Windows.Forms.Button
-    Friend WithEvents List_Score As System.Windows.Forms.ListBox
+    Friend WithEvents historyTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
 End Class
--- t3Final/t3Final/scoreMenu.vb
+++ t3Final/t3Final/scoreMenu.vb
@@ -1,49 +1,16 @@
-﻿Public Class scoreMenuForm
-    Private playerName As playerClass = New playerClass()
-    Private wins As Integer = 0
-    Private loss As Integer = 0
-    Private tie As Integer = 0
-    Private opponent As String
-
-
-
+﻿Imports System.IO
+Public Class scoreMenuForm
+    Private playerHistory As historyClass = New historyClass()
+    Private username As String = "UHD"
     'Holds the button to view
     Private Sub Btn_View_Click(sender As Object, e As EventArgs) Handles Btn_View.Click
-        Const intMax As Integer = 3
-        Try
-            Dim mauro As System.IO.StreamReader
-            ' Dim strFilename As String ' File name
-            Dim strFriend As String     ' Name of a friend
-            Dim strPhone As String      ' To hold a phone number
-            Dim intCount As Integer     ' Loop counter
-            ' Open the file.
-            mauro = My.Computer.FileSystem.OpenTextFileReader("Vader.txt")
-
-            ' Read the data.
-            For intCount = 1 To intMax
-                ' Read a name and phone number from the file.
-                strFriend = mauro.ReadLine()
-                strPhone = mauro.ReadLine()
-
-                ' Display the data in the list box.
-                List_Score.Items.Add("Wins: " & intCount.ToString())
-                List_Score.Items.Add("Loss: " & strFriend)
-                List_Score.Items.Add("Tie: " & strPhone)
-                List_Score.Items.Add("") ' Add a blank line
-            Next
-
-            ' Close the file.
-            mauro.Close()
-        Catch
-            MessageBox.Show("That file cannot be opened.")
-        End Try
+        historyTextBox.Text = File.ReadAllLines("nameofuser.txt").FirstOrDefault(Function(x) x.Contains(username)) ' this display the found username
     End Sub
-
 
 
     'Holds the Clear button
     Private Sub btn_Clear_Click(sender As Object, e As EventArgs) Handles btn_Clear.Click
-        List_Score.Items.Clear()
+        historyTextBox.Clear()
     End Sub
     'Button to go back or menu
     Private Sub btn_Back_Click(sender As Object, e As EventArgs) Handles btn_Back.Click
@@ -51,7 +18,22 @@
         mainMenuForm.Show()
     End Sub
 
-    Private Sub Score_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles historyTextBox.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub scoreMenuForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
